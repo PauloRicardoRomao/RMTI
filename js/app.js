098d36d07841e50abd80dbd70301acfe7ab9439e
PauloRicardoRomao <pauloromao.dev2020@gmail.com>
2025-07-21 22:08:09 -0300

    Botão orçamento, banners e layout

---
+++ js/app.js
@@ -0,0 +1,7 @@
+
+const btnWpp = document.getElementById('btn-orcamento');
+
+btnWpp.addEventListener('click', () => {
+    window.open('https://wa.me/55814998627852?text=Olá!%20Quero%20fazer%20um%20orçamento');
+});
+
